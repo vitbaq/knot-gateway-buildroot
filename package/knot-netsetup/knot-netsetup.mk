ea38008c0a9e85c3ffd804645ba00caf2ce32c28
Vitor Barros <vba@cesar.org.br>
2019-04-04 10:30:53 -0300

    package: Add knot-netsetup package
    This path add KNoT Gateway Network (OpenThread) setup service package.
    knot-netsetup is responsible for exports Bluetooth GATT service to
    allow users to setup openthread network parameters.
    
    Signed-off-by: Vitor Barros <vba@cesar.org.br>

---
+++ package/knot-netsetup/knot-netsetup.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# knot-netsetup
+#
+################################################################################
+
+KNOT_NETSETUP_VERSION = netsetup_vba_exp
+KNOT_NETSETUP_SITE = https://github.com/vitbaq/knot-gateway-netsetup.git
+KNOT_NETSETUP_SITE_METHOD = git
+KNOT_NETSETUP_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
